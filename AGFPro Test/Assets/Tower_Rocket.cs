99a314448a92b9ccfeae5197c0beec55abc22da3
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-05-18 00:11:37 +0100

    Script update for rocket

--- AGFPro Test/Assets/Tower_Rocket.cs
+++ AGFPro Test/Assets/Tower_Rocket.cs
@@ -1,19 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Tower_Rocket : MonoBehaviour
 {
 
-	// Components
-    
-    
+    // Variables
+    Rigidbody rigidBody;
+    [SerializeField] float rcsThrust = 30f;
+    [SerializeField] float mainThrust = 100f;
+
     // Use this for initialization
-	void Start ()
+    void Start ()
     {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        RocketControl();
 	}
+
+    private void RocketControl()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up * mainThrust);
+        }
+    }
 }
